b4a05487829e256ed6c3585b532d34ea26ba5a14
Suraj Kumar <85745631+suraj016@users.noreply.github.com>
2026-01-31 13:12:50 +0530

    Updated tree view

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,7 @@ function App() {
     <div className="app">
       <header className="app-header">
         <h1>Tree View Component</h1>
-        {/* <p className="subtitle">Drag & Drop • Lazy Loading • Inline Editing</p> */}
+      
       </header>
       <main className="app-main">
         <TreeView data={treeData} onDataChange={handleDataChange} />
